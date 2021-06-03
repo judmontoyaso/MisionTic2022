11f07551160d823b49b3c44788df7340a53c0587
judmontoyaso <judmontoyaso@unal.edu.co>
2021-06-02 20:16:24 -0500

    main

---
+++ Semana_3/Clase 2/ejercitacion.py
@@ -0,0 +1,12 @@
+# Hacer un juego de adivinanzas
+# El numero a adiviar es un aleatorio
+# entre 0 y 20
+#el codigo debe generar automaticamente el aleatorio
+#y preguntarle a l usuario con un input el numero que quiere adivinar
+#el juego debe tener un numero maximo de intentos.
+# si me paso del maximo sin adivinar, pierdo, si adibino gano
+
+
+#hacer un juego de piedra papel o tijera contra el computador.
+#El compjutador escoge aleatoriamente piedra papel o tijera y pedir al usuario su input.
+#el juego termina cuando uno de los dos gane 2 de tres partidas.
